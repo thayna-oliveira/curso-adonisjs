599fd0317a025c9626e0291833b341c9762b87f8
thayna-oliveira <oliveira.thayna@outlook.com>
2020-01-13 15:09:10 -0300

    aula 08 - resetando senha

---
+++ start/routes.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const Route = use('Route')
+
+
+Route.post('users', 'UserController.store')
+
+Route.post('sessions', 'SessionController.store')
+
+Route.post('passwords', 'ForgotPasswordController.store')
+
